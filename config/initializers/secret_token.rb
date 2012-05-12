978c9362a3f4b03086d644c050f8c2572a34c50f
Frau R <kristina_roddewig@arcor.de>
2012-05-12 15:50:19 +0200

    Created new Ruby on Rails application "ClearUp"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClearUp::Application.config.secret_token = '8c123fb00ef6b7990f72d8e0441db6cc7c002adb91523f58fbbbf64e283c0e98169fdd6aebcded63be14305338357eaaa915fd408f95a5b3f9771ba5d330f517'

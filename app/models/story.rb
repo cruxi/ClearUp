487a9e8380ee7245fed59444cf8d974e77abcf2e
Frau R <kristina_roddewig@arcor.de>
2012-05-12 17:46:49 +0200

    created scaffold story and added priority

---
+++ app/models/story.rb
@@ -0,0 +1,3 @@
+class Story < ActiveRecord::Base
+  attr_accessible :description, :title, :priority
+end
---
+++ db/migrate/20120512143652_story.rb
@@ -0,0 +1,7 @@
+class Story < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+end

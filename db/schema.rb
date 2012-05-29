3e26fa60825ac3a3ba26f782098e1b304685fb08
Frau R <kristina_roddewig@arcor.de>
2012-05-29 21:51:55 +0200

    reset db schema to add user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120523112044) do
+ActiveRecord::Schema.define(:version => 20120529192027) do
 
   create_table "stories", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,25 @@ ActiveRecord::Schema.define(:version => 20120523112044) do
     t.integer  "priority"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "login"
+    t.string   "email"
+    t.string   "persistence_token"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "single_access_token"
+    t.string   "perishable_token"
+    t.integer  "login_count"
+    t.integer  "failed_login_count"
+    t.datetime "last_request_at"
+    t.datetime "current_login_at"
+    t.datetime "last_login_at"
+    t.string   "current_login_ip"
+    t.string   "last_login_ip"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
   end
 
 end
